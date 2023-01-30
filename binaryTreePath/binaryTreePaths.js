f4d8689fa0e6bd348faff3d4330744d3578a3b12
javascriptsucks <evix1988@live.cn>
2023-01-29 23:30:41 -0600

    all-path-binary-tree-recursion

--- binaryTreePath/binaryTreePaths.js
+++ binaryTreePath/binaryTreePaths.js
@@ -1,5 +1,25 @@
-const binaryTreePaths=(root) => {
-  const res=[]
+const helper = function(node, currPath, res) {
+  currPath += '->' + node.val;
+  if (node.left === null && node.right === null) {
+    res.push(currPath);
+    return;
+  }
+  if (node.left) helper(node.left, currPath, res);
+  if (node.right) helper(node.right, currPath, res);
 
-  res.push.
-}
+};
+
+
+const binaryTreePaths = (root) => {
+  const res = [];
+  if (!root) return res;
+  let currPath = root.val.toString();
+  if (root.left === null && root.right === null) res.push(currPath);
+
+  if (root.left) helper(root.left, currPath, res);
+  if (root.right) helper(root.right, currPath, res);
+
+
+
+  return res;
+};
