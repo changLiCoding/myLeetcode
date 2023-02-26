44c93e260cdb835913d21a5791d00bf51d8bb1f3
javascriptsucks <evix1988@live.cn>
2023-02-25 22:37:54 -0600

    merge-two-sorted-lists

--- mergeTwoSortedLists/mergeTwoLists.js
+++ mergeTwoSortedLists/mergeTwoLists.js
@@ -1,3 +1,16 @@
-const mergeTwoLists = (list1, list2) => {
+const mergeTwoLists = function (list1, list2) {
+	let curr1 = list1;
+	let isOnList1 = true;
+	const stack = [list2];
 
+	while (stack.length) {
+		let curr2 = stack.shift();
+
+		if (curr2 === list2) {
+			let newNode = new ListNode(list2.val);
+			curr1.next = newNode;
+			newNode.next = list1.next;
+			curr1 = list1.next;
+		}
+	}
 };
