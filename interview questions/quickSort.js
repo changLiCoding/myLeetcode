77a6b7a721452da5bb3a14df4a7eb56c1442ced7
javascriptsucks <evix1988@live.cn>
2023-07-06 23:26:30 -0600

    update quickSort.js in js

--- interview questions/quickSort.js
+++ interview questions/quickSort.js
@@ -1,30 +1,47 @@
 const randomArray = [2.2, 41, 2, 5124, 561, 23, 5, 12, 3, 4];
 
-const swap= ( arr, left, right) => {
-  const temp=arr[left]
-  arr[left]=arr[right]
-  arr[right]=temp
-}
+const swap = (arr, left, right) => {
+	const temp = arr[left];
+	arr[left] = arr[right];
+	arr[right] = temp;
+};
 
-const partition=(arr, left, right) => {
-  const pivot=arr[right]
-  let leftIndex=left
-  let rightIndex=right-1
-  while (true) {
-    while(leftIndex < right && arr[leftIndex] <= pivot) leftIndex++
-  }
-  while (rightIndex>left&&arr[rightIndex]>=pivot) rightIndex--
-  if (leftIndex>=rightIndex) break
-  swap(arr, leftIndex, rightIndex)
+const partition = (arr, left, right) => {
+	const pivot = arr[Math.floor((left + right) / 2)];
+	let leftIndex = left;
+	let rightIndex = right;
+	while (leftIndex <= rightIndex) {
+		while (arr[leftIndex] < pivot) {
+			leftIndex++;
+		}
+		while (arr[rightIndex] > pivot) {
+			rightIndex--;
+		}
+		if (leftIndex <= rightIndex) {
+			swap(arr, leftIndex, rightIndex);
+			leftIndex++;
+			rightIndex--;
+		}
+	}
 
-swap(arr, leftIndex, right)
-return leftIndex
-}
+	return leftIndex;
+};
 
 const quickSort = (arr, left, right) => {
-	const partitionIndex = partition(arr, left, right);
-	quickSort(arr, left, partitionIndex - 1);
-	quickSort(arr, partitionIndex + 1, right);
+	let index;
+
+	if (arr.length > 1) {
+		index = partition(arr, left, right);
+
+		if (left < index - 1) {
+			quickSort(arr, left, index - 1);
+		}
+
+		if (index < right) {
+			quickSort(arr, index, right);
+		}
+	}
+	return arr;
 };
 
 console.log(quickSort(randomArray, 0, randomArray.length - 1));
