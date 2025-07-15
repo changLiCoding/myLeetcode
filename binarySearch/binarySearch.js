44f19a7baef3f4dec36a4212a79f248329d9cc8f
Chang Li <lichang9488@gmail.com>
2025-07-15 00:19:30 -0400

    Practise

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,19 +1,21 @@
 function binarySearch(nums, target) {
-  const n = nums.length;
+  const n = nums.length();
   let left = 0;
   let right = n - 1;
 
-  while (left < right) {
-    const mid = Math.floor(right - left);
+  while (left <= right) {
+
+    const mid = Math.floor((right + left) / 2);
 
     if (nums[mid] > target) {
-      right = mid - 1;
+      right = mid - 1
     } else if (nums[mid] < target) {
-      left = mid + 1;
+      left = mid + 1
     } else {
-      return mid;
+      return mid
     }
-  }
 
-  return -1;
+
+  }
+  return -1
 }
