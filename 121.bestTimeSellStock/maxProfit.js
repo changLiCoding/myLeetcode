94a499880c8cf1d34e5e78fe88d32bee483c8cf6
Chang Li <lichang9488@gmail.com>
2025-07-22 23:24:29 -0400

    Max profit + binary search

--- 121.bestTimeSellStock/maxProfit.js
+++ 121.bestTimeSellStock/maxProfit.js
@@ -1,22 +1,42 @@
-function maxProfit(prices) {
+// function maxProfit(prices) {
 
-  let max = 0;
-  let b = 0;
-  let s = 1;
+//   let max = 0;
+//   let b = 0;
+//   let s = 1;
 
-  while (s < prices.length && b < s) {
-    const profit = prices[s] - prices[b];
+//   while (s < prices.length && b < s) {
+//     const profit = prices[s] - prices[b];
 
-    max = Math.max(max, profit);
+//     max = Math.max(max, profit);
 
-    if (prices[s] > prices[b]) {
-      s++
-    } else {
-      b = s;
-      s++
-    }
+//     if (prices[s] > prices[b]) {
+//       s++
+//     } else {
+//       b = s;
+//       s++
+//     }
+//   }
+
+//   return max
+
+// }
+
+
+const bestDeal = (nums) => {
+  let left = 0, right = 1;
+  const res = [];
+  let max = - Infinity;
+  while (right < nums.length && left < right) {
+    const current = nums[right] - nums[left];
+    max = Math.max(max, current);
+      if (nums[left] > nums[right]) {
+        left = right;
+        right++;
+      } else {
+        right++;
+      }
+
   }
 
   return max
-
 }
--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,20 +1,38 @@
-function binarySearch (nums,target) {
+// function binarySearch (nums,target) {
 
-  let l = 0;
-  let r = nums.length();
+//   let l = 0;
+//   let r = nums.length();
 
-  while (l <= r) {
-    const mid = Math.floor((r + l) / 2);
+//   while (l <= r) {
+//     const mid = Math.floor((r + l) / 2);
 
-    if (nums[mid] > target ) {
+//     if (nums[mid] > target ) {
+//       right = mid - 1;
+//     } else if (nums[mid] < target) {
+//       left = mid + 1;
+//     } else {
+//       return mid
+//     }
+//   }
+//   return -1;
+// }
+
+
+
+const binarySearch = (nums, target) => {
+  let left = 0, right = nums.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((right + left) / 2);
+
+    if (nums[mid] > target) {
       right = mid - 1;
     } else if (nums[mid] < target) {
       left = mid + 1;
     } else {
-      return mid
+      return mid;
     }
   }
+
   return -1;
 }
-
-
