6901b37eba6b36eeb777b4639c10ab31177a0270
Chang Li <lichang9488@gmail.com>
2025-07-26 00:24:25 -0400

    Practise

--- 167twoIntSum/twoIntSum.js
+++ 167twoIntSum/twoIntSum.js
@@ -1,13 +1,25 @@
 function twoSum(nums, target) {
-  const numMap = new Map();
+  // const numMap = new Map();
 
-  for (let i in nums) {
-    const targetNum = target - nums[i];
+  // for (let i in nums) {
+  //   const targetNum = target - nums[i];
 
-    if (numMap.has(targetNum)) {
-      return [parseInt(numMap.get(targetNum)) + 1 , parseInt(i) + 1]
+  //   if (numMap.has(targetNum)) {
+  //     return [parseInt(numMap.get(targetNum)) + 1 , parseInt(i) + 1]
+  //   }
+
+  //   numMap.set(nums[i], parseInt(i));
+  // }
+
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const theOtherNum = target - nums[i];
+
+    if (map.has(theOtherNum)) {
+      return [i, map.get(theOtherNum)]
     }
 
-    numMap.set(nums[i], parseInt(i));
+    map.set(nums[i], i);
   }
 }
--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -18,21 +18,38 @@
 // }
 
 
+function binarySearch (nums, target) {
+  let l = 0, r = nums.length - 1;
 
-const binarySearch = (nums, target) => {
-  let left = 0, right = nums.length - 1;
-
-  while (left <= right) {
-    const mid = Math.floor((right + left) / 2);
+  while (l <= r) {
+    const mid = Math.floor((r + l) / 2)
 
     if (nums[mid] > target) {
-      right = mid - 1;
+      r = mid - 1;
     } else if (nums[mid] < target) {
-      left = mid + 1;
+      l = mid + 1;
     } else {
       return mid;
     }
   }
-
-  return -1;
+  return -1
 }
+
+
+// const binarySearch = (nums, target) => {
+//   let left = 0, right = nums.length - 1;
+
+//   while (left <= right) {
+//     const mid = Math.floor((right + left) / 2);
+
+//     if (nums[mid] > target) {
+//       right = mid - 1;
+//     } else if (nums[mid] < target) {
+//       left = mid + 1;
+//     } else {
+//       return mid;
+//     }
+//   }
+
+//   return -1;
+// }
