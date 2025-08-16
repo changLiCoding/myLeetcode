08301ead990893d42c3e895756d66b857521bc7b
Chang Li <lichang9488@gmail.com>
2025-08-16 00:07:17 -0400

    Binary-search

--- 153.findMinBinarySearch/findMin.js
+++ 153.findMinBinarySearch/findMin.js
@@ -21,3 +21,29 @@ function findMin(nums) {
 
   return min;
 }
+
+function binaryFindMin(arr) {
+  let min = (min = arr[0]);
+  let l = 0,
+    r = arr.length - 1;
+
+  while (l <= r) {
+    if (arr[l] <= arr[r]) {
+      min = Math.min(min, arr[l]);
+      break;
+    }
+
+    const mid = Math.floor((l + r) / 2);
+    const current = arr[mid];
+
+    min = Math.min(min, current);
+
+    if (current >= arr[l]) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  return min;
+}
--- 33.searchInRotatedSortedArray/binarySearch.js
+++ 33.searchInRotatedSortedArray/binarySearch.js
@@ -66,3 +66,29 @@ const search = (arr, target) => {
 
   return -1;
 }
+
+const search2 = (arr, target) => {
+  let l = 0, r = arr.length - 1;
+
+  while (l <= r) {
+    const mid = Math.floor((r + l) / 2);
+    if (arr[mid] === target) return mid;
+    if (arr[mid] >= arr[l]) {
+      if (target > arr[mid] || target < arr[l]) {
+        l = mid + 1;
+
+      } else {
+        r = mid - 1;
+      }
+
+    } else {
+      if (target < arr[mid] || target > arr[r] ) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+  }
+
+  return -1;
+}
