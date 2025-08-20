bf813c527ad9c1ccd6f300e258214f39613bd01f
Chang Li <lichang9488@gmail.com>
2025-08-19 21:55:34 -0400

    Recursion link list

--- 206.revertLinkList/revert.js
+++ 206.revertLinkList/revert.js
@@ -11,3 +11,15 @@ function revert (header) {
 
   return prev
 }
+
+function recursion (header) {
+  if (!header) return null;
+  const newHeader = header;
+
+  if (header.next) {
+    newHeader = this.recursion(header.next);
+    header.next.next = header
+  }
+  header.next = null;
+  return newHeader;
+}
