88e57e47dd0f9377013644056f39406ca1969022
Chang Li <lichang9488@gmail.com>
2025-08-27 00:51:13 -0400

    Practise

--- 153.findMinBinarySearch/findMin.js
+++ 153.findMinBinarySearch/findMin.js
@@ -98,3 +98,30 @@ function findMinBinary (arr, target) {
 
   return -1;
 }
+
+function findTarget (arr, target) {
+  let l = 0, r = arr.length - 1;
+
+  while (l <= r) {
+    const mid = Math.floor((l + r) /2);
+
+    if (arr[mid]===target) return mid;
+
+    if (arr[mid] >= arr[l]) {
+      if (target > arr[mid] || target < arr[l]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1
+      }
+    } else {
+      if (target < arr[mid] || target > arr[r]) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+  }
+
+
+  return -1;
+}
