c1bcbe21c499e7d40c7b9792580661ab58f3bf44
Chang Li <lichang9488@gmail.com>
2025-08-28 22:22:34 -0400

    Practise

--- 21.mergeLinkList/mergeLinkList.js
+++ 21.mergeLinkList/mergeLinkList.js
@@ -37,6 +37,35 @@ function mergeList (head1, head2) {
 
     curr = curr.next;
   }
+    if (list1) curr.next = list1;
+    if (list2) curr.next = list2;
+
 
   return dummy.next
+}
+
+function mergeTwoLists (list1, list2) {
+  const dummy = {val: 0, next: null};
+  let cur = dummy;
+
+  while (list1 && list2) {
+    if (list1.val <= list2.val) {
+      cur.next = list1;
+      list1 = list1.next;
+    } else {
+      cur.next = list2;
+      list2 = list2.next
+    }
+
+    cur = cur.next;
+  }
+
+  if (list1) {
+    cur.next = list1;
+  }
+  if (list2) {
+    cur.next = list2;
+  }
+
+  return dummy.next;
 }
