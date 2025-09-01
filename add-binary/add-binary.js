c2413f69e76248b87ffeaa4402cac89bdcba8150
Chang Li <lichang9488@gmail.com>
2025-08-31 23:49:32 -0400

    Practise

--- add-binary/add-binary.js
+++ add-binary/add-binary.js
@@ -26,3 +26,20 @@ const addBinary = function(a, b) {
 };
 
 console.log(addBinary('101111', '10'));
+
+
+function binarySearch (arr, target) {
+  let l = 0, r = arr.length - 1;
+  while (l < r) {
+    const mid = Math.floor((r + l) /2);
+    const val = arr[mid];
+    if (target > val) {
+      l = mid + 1;
+    } else if (target < val ) {
+      r = mid - 1;
+    } else {
+       return mid;
+    }
+  }
+  return - 1;
+}
