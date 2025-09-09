9a7d846a03f7b799eb5f450ecd9d16b4bb74a4bf
Chang Li <lichang9488@gmail.com>
2025-09-08 23:34:57 -0400

    Practise

--- 153.findMinBinarySearch/findMin.js
+++ 153.findMinBinarySearch/findMin.js
@@ -125,3 +125,30 @@ function findTarget (arr, target) {
 
   return -1;
 }
+
+function findTargetInRotatedSortedList (arr, target) {
+  let l = 0, r = arr.length - 1;
+
+  while (l <= r) {
+    const mid = Math.floor((l + r) /2);
+
+    if (arr[mid]===target) return mid;
+
+    if (arr[mid] >= arr[l]) {
+      if (target > arr[mid] || target < arr[l]) {
+        l = mid + 1;
+      } else {
+        r = mid - 1
+      }
+    } else {
+      if (target < arr[mid] || target > arr[r]) {
+        r = mid - 1;
+      } else {
+        l = mid + 1;
+      }
+    }
+  }
+
+
+  return -1;
+}
