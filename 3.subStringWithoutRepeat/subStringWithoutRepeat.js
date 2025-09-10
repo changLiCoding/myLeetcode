45483c9648155549e6dd2f8f2b0dc545a63bef20
Chang Li <lichang9488@gmail.com>
2025-09-09 22:09:53 -0400

    Practise

--- 3.subStringWithoutRepeat/subStringWithoutRepeat.js
+++ 3.subStringWithoutRepeat/subStringWithoutRepeat.js
@@ -1,4 +1,4 @@
-function lengthOfLongestSubstring (s) {
+function lengthOfLongestSubstring(s) {
   if (s.length <= 1) return s.length;
 
   let l = 0;
@@ -6,18 +6,34 @@ function lengthOfLongestSubstring (s) {
   const sSet = new Set();
 
   for (let r = 0; r < s.length; r++) {
-    
-    while(sSet.has(s[r])) {
+    while (sSet.has(s[r])) {
       sSet.delete(s[l]);
       l++;
     }
-
 
     sSet.add(s[r]);
     const currentLength = r - l + 1;
     maxLength = Math.max(maxLength, currentLength);
   }
 
+  return maxLength;
+}
 
-  return maxLength
+function lengthOfLongest(s) {
+  let l = 0;
+  let maxLength = 0;
+  const sSet = new Set();
+
+  for (let r = 0; r < s.length; r++) {
+    while (sSet.has(s[r])) {
+      sSet.delete(s[l]);
+      l++;
+    }
+
+    sSet.add(s[r]);
+    const currentLength = r - l + 1;
+    maxLength = Math.max(maxLength, currentLength);
+  }
+
+  return maxLength;
 }
